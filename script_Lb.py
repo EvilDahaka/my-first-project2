8caeb904d1102f6398aca689c2e8138dd0df9da7
MrArgon-coder <lozovukba@gmail.com>
2024-09-13 09:04:11 +0300

    Create script_Lb.py

---
+++ script_Lb.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+num3 = float(input("Введіть третє число: "))
+
+
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+
+print(f"Найбільше число: {largest}")
